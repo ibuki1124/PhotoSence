8924477de54465d08430e2703cf3d3f6f95190bf
ibuki1124 <132910745+ibuki1124@users.noreply.github.com>
2024-07-14 08:31:29 +0000

    [Add] 投稿に関する大まかな設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,13 @@
 class Post < ApplicationRecord
   belongs_to :user
+  
+  has_one_attached :vision_image
+  
+  def get_vision_image(width, height)
+    unless vision_image.attached?
+      file_path = Rails.root.join("app/assets/images/no.jpg")
+      vision_image.attach(io: File.open(file_path), filename: "no-image.jpg", content_type: "image/jpeg")
+    end
+    vision_image.variant(resize_to_limit: [width, height]).processed
+  end
 end
