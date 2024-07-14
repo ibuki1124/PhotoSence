88e26ab05e8df7005b75d9fe117c0d00a2599998
ibuki1124 <132910745+ibuki1124@users.noreply.github.com>
2024-07-14 08:52:52 +0000

    [Update] postsテーブルのuseer_idの型

---
+++ db/migrate/20240714084839_change_datatype.rb
@@ -0,0 +1,5 @@
+class ChangeDatatype < ActiveRecord::Migration[6.1]
+  def change
+    change_column :posts, :user_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_07_14_073907) do
+ActiveRecord::Schema.define(version: 2024_07_14_084839) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -42,7 +42,7 @@ ActiveRecord::Schema.define(version: 2024_07_14_073907) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
-    t.string "user_id"
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
