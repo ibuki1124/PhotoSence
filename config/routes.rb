7ac88909f4272e7710744730e01f631c858c730b
ibuki1124 <132910745+ibuki1124@users.noreply.github.com>
2024-07-14 06:38:03 +0000

    [Add] postsコントローラ・ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root "posts#top"
+  
+  resources :posts, only: [:new, :index, :show, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
