bc4b30379b416959eef9a47d5151b44f0d4cea18
ibuki1124 <132910745+ibuki1124@users.noreply.github.com>
2024-08-12 13:10:01 +0000

    [Add] コメント機能

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :tags, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :comments, dependent: :destroy
   
   has_one_attached :vision_image
   
