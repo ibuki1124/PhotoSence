18fa194e9f20660eb071e40c5f5f6d1d4451760e
ibuki1124 <132910745+ibuki1124@users.noreply.github.com>
2024-08-12 14:35:03 +0000

    [Add] コメント投稿失敗時のエラーメッセージ

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,14 @@
 class CommentsController < ApplicationController
   def create
-    post = Post.find(params[:post_id])
-    comment = Comment.new(comment_params)
-    comment.user_id = current_user.id
-    comment.post_id = post.id
-    comment.save
-    redirect_to post_path(post)
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new(comment_params)
+    @comment.user_id = current_user.id
+    @comment.post_id = @post.id
+    if @comment.save
+      redirect_to post_path(@post)
+    else
+      render "posts/show"
+    end
   end
   
   def destroy
